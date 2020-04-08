087d3c2c91e8a82a6b63d13245e55648821d2693
remylouisew <remylouisew@gmail.com>
2020-04-08 13:29:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,12 @@
 #   !  /bin/bash
 #   sudo apt-get update
 #   sudo apt-get install python3-pip
-#   sudo apt-get update
 #   sudo apt-get install git
 #   pip3 install tweepy
 #   pip3 install google-cloud-storage
 #   pip3 install google-cloud-pubsub
+#   mkdir /home/remyw/keys
+#   gsutil cp -r gs://mycredentials-rw/ /home/remyw/keys
 #   2) put your twitter developer tokens/secrets in a GCS bucket (or copy them to the VM by some other means)
 #   3) execute: git clone https://github.com/remylouisew/twitter_streaming.git
 #   4) run this file: $python3 /home/remyw/twitter_streaming/app.py
@@ -21,8 +22,6 @@ import tweepy
 from google.cloud import pubsub_v1 as pub
 from google.cloud import storage as gcs
 
-!mkdir /home/remyw/keys
-!gsutil cp -r gs://mycredentials-rw/ /home/remyw/keys
 #!gsutil cp -r gs://mycredentials-rw/keysecret.txt /home/remyw/keysecret.txt
 #!gsutil cp -r gs://mycredentials-rw/token.txt /home/remyw/token.txt
 #!gsutil cp -r gs://mycredentials-rw/tokensecret.txt /home/remyw/tokensecret.txt
